2394752748125eed2fefc46c605547834ed03673
jsy2026 <jungseiyoon1@gmail.com>
2026-02-01 00:55:27 +0900

    도착지점, 밧줄, 타이머

---
+++ Assets/Scripts/GoalPoint.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+    [Header("UI Settings")]
+    [SerializeField] private GameObject clearMessageUI; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ShowClearMessage();
+        }
+    }
+
+    private void ShowClearMessage()
+    {
+        if (clearMessageUI != null)
+        {
+            clearMessageUI.SetActive(true); 
+        }
+    }
+}
