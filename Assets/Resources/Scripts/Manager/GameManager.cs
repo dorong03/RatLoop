3bb60cfe3f4d24720285dac586b056874feb9a3e
qoralstmd6825 <qoralstmd6825@gmail.com>
2026-06-17 12:04:17 +0900

    스테이지 그래픽 개선

--- Assets/Resources/Scripts/Manager/GameManager.cs
+++ Assets/Resources/Scripts/Manager/GameManager.cs
@@ -61,8 +61,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         Cursor.visible = false;
-        PlayerPrefs.SetInt("MaxClearLevelID", 8);
-        maxClearLevelID = PlayerPrefs.GetInt("MaxClearLevelID", 8);
+        maxClearLevelID = PlayerPrefs.GetInt("MaxClearLevelID", 1);
     }
     
     private void Update()
