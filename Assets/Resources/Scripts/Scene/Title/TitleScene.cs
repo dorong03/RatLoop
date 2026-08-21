902061474a71a1bb261ac216a80e2933026cfad0
dorong03 <qoralstmd6825@google.com>
2026-08-21 12:04:39 +0900

    스테이지 선택창 진입 애니메이션 게임 실행 후 한번만 출력되게 변경

--- Assets/Resources/Scripts/Scene/Title/TitleScene.cs
+++ Assets/Resources/Scripts/Scene/Title/TitleScene.cs
@@ -1,14 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class TitleScene : MonoBehaviour
 {
+    private static bool IsAnimationPlayed;
+    
     [SerializeField] private GameObject startImage;
     [SerializeField] private GameObject exitImage;
     [SerializeField] private UIAnimation animationUI;
 
     private bool selectedStart = true;
     private bool isLoading = false;
+
+    // 도메인 리로드를 끈 상태에서도 게임을 새로 실행할 때마다 초기화되도록 한다.
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    private static void ResetStatics()
+    {
+        IsAnimationPlayed = false;
+    }
 
     private void Start()
     {
@@ -44,7 +54,15 @@ public class TitleScene : MonoBehaviour
             if (selectedStart)
             {
                 isLoading = true;
-                animationUI.PlayAnimation("LevelSelection");
+                if (IsAnimationPlayed)
+                {
+                    SceneManager.LoadScene("LevelSelection");
+                }
+                else
+                {
+                    IsAnimationPlayed = true;
+                    animationUI.PlayAnimation("LevelSelection");
+                }
             }
             else
             {
